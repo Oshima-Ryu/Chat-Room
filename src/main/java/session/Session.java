e76a209dba2e76df2a06fef57097a648f4f8b29d
Oshima-Ryu <taylor233@sina.com>
2019-02-07 23:33:04 +0800

    add session code

--- src/main/java/session/Session.java
+++ src/main/java/session/Session.java
@@ -1,4 +1,20 @@
 package session;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class Session {
+    private String userId;
+    private String userName;
+
+    public Session(String userId, String userName){
+        this.userId = userId;
+        this.userName = userName;
+    }
+
+    public String toString(){
+        return userId + ":" + userName;
+    }
 }
